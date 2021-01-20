c1f2ae1d7123b7e7c6b2ecaef3aa7427ec8fbbbd
michalsidzej <michalsidzej@gmail.com>
2021-01-20 16:42:08 +0100

    cleared files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import './dom'
+
 
 console.log('index file')
-
-const greet = (name) => {
-  console.log(`hello ${name}`)
-}
-
-greet('mario')
-greet('luigi')
-greet('yoshi')
-
-class User {
-  constructor(){
-    this.score = 0;
-  }
-}
